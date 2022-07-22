eca85d579eee080585926e2e6e96e5b0b79336db
David Onuh <davidonuh1@gmail.com>
2022-07-22 22:56:54 +0100

    create models for accounts, courses, subject and modules

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+
+    is_student = models.BooleanField(default=True)
+    is_teacher = models.BooleanField(default=False)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,38 @@
+from turtle import title
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+
+
+class Subject(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Course(models.Model):
+
+    teacher = models.ForeignKey(
+        CustomUser, related_name="course_by", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} by {self.teacher}"
+
+
+class Module(models.Model):
+
+    course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    text = models.TextField(verbose_name="module_text")
+    content = models.FileField(upload_to="videos/")
+
+    def __str__(self) -> str:
+        return self.title
